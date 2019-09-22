d0484a5f8f3bdd383574e9a9be3d6edd6c1b3b3a
zak-opus <akak7@foxmail.com>
2019-09-22 18:15:57 +0800

    Vue son component

---
+++ TodoItem.vue
@@ -0,0 +1,21 @@
+  <!-- eslint-disable no-new  -->
+<template>
+<li class="item" @click="handleDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props: ['content','index'],
+  methods:{
+    handleDelete:function(){
+      this.$emit('delete',this.index)
+    }
+  }
+}
+</script>
+
+<style>
+.item{
+  color:red;
+}
+</style>
